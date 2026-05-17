dd148cd333c3bfe1edb0f1a661c0261c91534493
Ola Philips. <olaphilips310@gmail.com>
2026-05-17 23:56:00 +0100

    Update Completed.jsx

--- src/components/Completed.jsx
+++ src/components/Completed.jsx
@@ -1,4 +1,9 @@
-const Completed = () => {
+const Completed = ({tasks}) => {
+
+  const completedTasks = tasks.filter(
+    (task) => task.status === "Completed"
+  );
+
     return(
         <>
              <div className="bg-white rounded-2xl shadow-lg p-5">
@@ -8,47 +13,46 @@ const Completed = () => {
               </h2>
 
               <span className="bg-green-100 text-green-700 text-sm px-3 py-1 rounded-full">
-                8 Tasks
+
+                {completedTasks.length} Tasks
+
               </span>
             </div>
 
             <div className="space-y-4">
-
+              {completedTasks.map((task) => ( 
               <div className="border border-green-200 bg-green-50 rounded-2xl p-4">
                 <div className="flex items-start justify-between gap-3">
                   <div>
                     <h3 className="font-semibold text-gray-800 text-lg line-through">
-                      API Integration
+                     {task.title}
                     </h3>
 
                     <p className="text-sm text-gray-500 mt-1">
-                      Successfully connected backend APIs.
+                      {task.description}
                     </p>
                   </div>
 
                   <span className="bg-green-200 text-green-700 text-xs px-3 py-1 rounded-full whitespace-nowrap">
-                    Done
+                    {task.priority}
                   </span>
-                </div>
+                                </div>
+
+              <div className="flex items-center justify-between mt-4">
+
+                <span className="text-sm text-gray-400">
+                  Completed on: {task.date}
+                </span>
+
+                <button className="text-blue-600 hover:text-blue-800 text-sm font-medium">
+                  View
+                </button>
+
               </div>
 
-              <div className="border border-green-200 bg-green-50 rounded-2xl p-4">
-                <div className="flex items-start justify-between gap-3">
-                  <div>
-                    <h3 className="font-semibold text-gray-800 text-lg line-through">
-                      Authentication UI
-                    </h3>
-
-                    <p className="text-sm text-gray-500 mt-1">
-                      Finished login and signup pages.
-                    </p>
-                  </div>
-
-                  <span className="bg-green-200 text-green-700 text-xs px-3 py-1 rounded-full whitespace-nowrap">
-                    Done
-                  </span>
-                </div>
-              </div>
+            </div>
+        
+                    ))} 
 
             </div>
           </div>
